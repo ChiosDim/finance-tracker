18422580286bc97b5a8cc5072c81ff06aac15dd1
Dimitris Chios <chiosdimit@gmail.com>
2026-06-12 10:32:07 +0300

    Add WebAssembly support for Microsoft.AspNetCore.Components.Forms

--- frontend/obj/Debug/net9.0/frontend.AssemblyInfo.cs
+++ frontend/obj/Debug/net9.0/frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32356bc56fbca5cba0fd90c48028b1cb57f15bab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+779ac2c38fe87ca45fd475d14fadbb8e88c7a612")]
 [assembly: System.Reflection.AssemblyProductAttribute("frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- frontend/obj/Release/net9.0/frontend.AssemblyInfo.cs
+++ frontend/obj/Release/net9.0/frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cd6150de191e624ab7cf690a5798cf15ea2235a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+779ac2c38fe87ca45fd475d14fadbb8e88c7a612")]
 [assembly: System.Reflection.AssemblyProductAttribute("frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
